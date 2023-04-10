f6aa26ef1b1261ec7a4069d58e6db8e4e65bba01
Andrei Razlivskii <derazliv@gmail.com>
2023-04-10 08:17:06 +0300

    yes

--- Task4.py
+++ Task4.py
@@ -10,52 +10,58 @@ def factorial(num):
 def arccos(num):
     return math.acos(num)
 
-operation = input("Введите символ операции (+, -, /, *, ^, mod, rand, !, acos): ")
+while (True):  ##начало цикла
 
-if operation == "+":
+
+
+ operation = input("Введите символ операции (+, -, /, *, ^, mod, rand, !, acos,exit): ")
+
+ if operation == "+": ##сложение
     num1 = float(input("Введите первое число: "))
     num2 = float(input("Введите второе число: "))
     result = num1 + num2
     print("Результат: ", result)
-elif operation == "-":
-    num1 = float(input("Введите первое число: "))
-    num2 = float(input("Введите второе число: "))
-    result = num1 - num2
-    print("Результат: ", result)
-elif operation == "/":
-    num1 = float(input("Введите первое число: "))
-    num2 = float(input("Введите второе число: "))
-    if num2 == 0:
+ elif operation == "-": ##вычитание
+     num1 = float(input("Введите первое число: "))
+     num2 = float(input("Введите второе число: "))
+     result = num1 - num2
+     print("Результат: ", result)
+ elif operation == "/":##деление
+     num1 = float(input("Введите первое число: "))
+     num2 = float(input("Введите второе число: "))
+     if num2 == 0:
         print('Делить на ноль нельзя')
-    result = num1 / num2
-    print("Результат: ", result)
-elif operation == "*":
-    num1 = float(input("Введите первое число: "))
-    num2 = float(input("Введите второе число: "))
-    result = num1 * num2
-    print("Результат: ", result)
-elif operation == "^":
-    num1 = float(input("Введите число: "))
-    power = float(input("Введите степень: "))
-    result = num1 ** power
-    print("Результат: ", result)
-elif operation == "mod":
-    num1 = float(input("Введите число: "))
-    mod = float(input("Введите делитель: "))
-    result = num1 % mod
-    print("Результат: ", result)
-elif operation == "rand":
-    low = float(input("Введите нижнюю границу диапазона: "))
-    high = float(input("Введите верхнюю границу диапазона: "))
-    result = random.uniform(low, high)
-    print("Результат: ", result)
-elif operation == "!":
-    num = int(input("Введите число для вычисления факториала: "))
-    result = factorial(num)
-    print("Результат: ", result)
-elif operation == "acos":
-    num = float(input("Введите число для вычисления арккосинуса: "))
-    result = arccos(num)
-    print("Результат: ", result)
-else:
-    print("Неверный символ операции")
+     result = num1 / num2
+     print("Результат: ", result)
+ elif operation == "*":##умножение
+     num1 = float(input("Введите первое число: "))
+     num2 = float(input("Введите второе число: "))
+     result = num1 * num2
+     print("Результат: ", result)
+ elif operation == "^":##возведение в степень
+     num1 = float(input("Введите число: "))
+     power = float(input("Введите степень: "))
+     result = num1 ** power
+     print("Результат: ", result)
+ elif operation == "mod":##остаток от деления
+     num1 = float(input("Введите число: "))
+     mod = float(input("Введите делитель: "))
+     result = num1 % mod
+     print("Результат: ", result)
+ elif operation == "rand":##рандомизация из диапозона
+     low = float(input("Введите нижнюю границу диапазона: "))
+     high = float(input("Введите верхнюю границу диапазона: "))
+     result = random.uniform(low, high)
+     print("Результат: ", result)
+ elif operation == "!":##факториал
+     num = int(input("Введите число для вычисления факториала: "))
+     result = factorial(num)
+     print("Результат: ", result)
+ elif operation == "acos":##арккосинус
+     num = float(input("Введите число для вычисления арккосинуса: "))
+     result = arccos(num)
+     print("Результат: ", result)
+ elif operation == "exit":##Выход из цикла при надобности
+     break
+ else:
+     print("Неверный символ операции")
